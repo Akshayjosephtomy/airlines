fe2da5b30a4cb04f7ddc7dbac160b5dc78290d1f
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-11 16:55:30 +0530

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AirlinesComponent } from './airlines/airlines.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const link:Routes=[
+  {
+    path:"",component:AirlinesComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { AirlinesComponent } from './airlines/airlines.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
